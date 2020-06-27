7ecba5e2979119a58d2171d27cd9285fd83af51c
yin.shi <yin.shi@btcmyc.com>
2020-06-28 00:42:39 +0800

    init

---
+++ src/main/java/cn/office/tools/meetingroom/Startup.java
@@ -0,0 +1,10 @@
+package cn.office.tools.meetingroom;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+    public static void main(String[] args) {
+        SpringBootApplication.Run(Startup.class, args);
+    }
+}
