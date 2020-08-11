d36dbf0ad524844d45573b1f1bbb40020e21f72d
Yongming Xia <yongming.xia@partner.bmw.com>
2020-08-12 01:27:04 +0800

    add meetingroom management

---
+++ src/main/java/cn/office/tools/usercenter/controller/MeetingRoomController.java
@@ -0,0 +1,36 @@
+package cn.office.tools.usercenter.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import cn.office.tools.usercenter.service.MeetingRoomService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Controller
+@EnableAutoConfiguration
+@RequestMapping("/meeting")
+@Api(tags = "会议管理")
+//@PreAuthorize("hasRole('Admin')")
+public class MeetingRoomController {
+    @Autowired
+    private TemplateEngine engine;
+    @Autowired
+    private MeetingRoomService meetingService;
+
+    @GetMapping("/rooms")
+    @ResponseBody
+    @ApiOperation("会议列表")
+    //@ApiImplicitParam(name = "id", value = "id", defaultValue = "99", required = true)
+    public String Index() {
+        System.out.println(meetingService.getAllMeetingRooms());
+        Context ctx = new Context();
+        return engine.process("/meeting/rooms/index", ctx);
+    }
+}
--- src/main/java/cn/office/tools/usercenter/service/MeetingRoomService.java
+++ src/main/java/cn/office/tools/usercenter/service/MeetingRoomService.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 
 import cn.office.tools.usercenter.dao.MeetingRoom;
 //imprort cn.office.tools.usercenter.dao.;
+import cn.office.tools.usercenter.dao.RoomTimeperiod;
 
 @Service
 public interface MeetingRoomService {
     List<MeetingRoom> getAllMeetingRooms();  
+    List<RoomTimeperiod> getAllTimeperiod();
 }
