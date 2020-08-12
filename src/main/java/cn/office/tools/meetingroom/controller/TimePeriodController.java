f5ac92f97b9b5b283db4fa909f08656a1e636f1c
Yongming Xia <yongming.xia@partner.bmw.com>
2020-08-12 18:04:06 +0800

    add roomtimeperiod contoller

--- src/main/java/cn/office/tools/meetingroom/controller/TimePeriodController.java
+++ src/main/java/cn/office/tools/meetingroom/controller/TimePeriodController.java
@@ -3,24 +3,22 @@ package cn.office.tools.meetingroom.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import cn.office.tools.meetingroom.dao.MeetingRoom;
+import cn.office.tools.meetingroom.dao.RoomTimeperiod;
 import cn.office.tools.meetingroom.service.MeetingRoomService;
 
 @RestController
-@RequestMapping("/rooms")
-public class RoomController {
+@RequestMapping("/times")
+public class TimePeriodController {
     @Autowired
     private MeetingRoomService meetingService;
 
     @RequestMapping(value="/", method = RequestMethod.GET)
-    public List<MeetingRoom> getRooms() {
-        return meetingService.getAllMeetingRooms();
+    public List<RoomTimeperiod> getTimePeriod() {
+        return meetingService.getAllTimeperiod();
     }
-    
+
 }
